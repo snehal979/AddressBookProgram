7b7cf34563486d0e9742f4ad0f1a3763b2d8a6ef
Snehal979 <bansodsnehal535@gmail.com>
2022-12-12 21:41:13 +0530

    (Snehal)Add: Welcome to Address book

---
+++ AddressBookProgram/AddressBookProgram/DataOfPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    public class DataOfPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNUmber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ AddressBookProgram/AddressBookProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AddressBookProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcom to Address Book Project");
+        }
+    }
+}
