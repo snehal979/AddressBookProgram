220000c52407ae26bc9aa7b9e7cf220362c67bc6
Snehal979 <bansodsnehal535@gmail.com>
2022-12-12 21:51:24 +0530

    (SNehal)Add: creat a new contact and add to the address book

---
+++ AddressBookProgram/AddressBookProgram/AddressBookMainPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    public class AddressBookMainPage
+    {
+        List<DataOfPerson> addresslist = new List<DataOfPerson>();
+        public void NewCreateContact()
+        {
+            DataOfPerson contact = new DataOfPerson();
+
+            Console.WriteLine("Enter the firstName");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter the lastName");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter the Address");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter the City");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter the State");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter the Zip");
+            contact.Zip = Console.ReadLine();
+            Console.WriteLine("Enter the PhoneNumber");
+            contact.PhoneNUmber = Console.ReadLine();
+            Console.WriteLine("Enter the Email ID");
+            contact.Email = Console.ReadLine();
+
+            addresslist.Add(contact);    // add data into the list of address book
+
+        }
+    }
+
+}
--- AddressBookProgram/AddressBookProgram/Program.cs
+++ AddressBookProgram/AddressBookProgram/Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcom to Address Book Project");
+            AddressBookMainPage page = new AddressBookMainPage();
+            page.NewCreateContact();
 
         }
     }
